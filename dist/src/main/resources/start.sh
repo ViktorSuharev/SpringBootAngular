7429354db3b86e056e722cf05a8386e6bc6c8914
viktorsuharev <viktor.suhareff@yandex.ru>
2018-10-14 15:26:43 +0300

    add dist module

--- dist/src/main/resources/start.sh
+++ dist/src/main/resources/start.sh
@@ -2,5 +2,5 @@
 BIN_DIR=$(cd $(dirname $0);echo $PWD)
 APP_HOME=$(dirname $BIN_DIR)
 cd $APP_HOME
-nohup java -jar $APP_HOME/lib/${project.artifactId}-${project.version}.jar > /dev/null 2>&1 &
+nohup java -jar $APP_HOME/lib/app.jar > /dev/null 2>&1 &
 echo $! > $APP_HOME/bin/app.pid
